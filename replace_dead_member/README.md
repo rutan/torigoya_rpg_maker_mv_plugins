a9ee4e6c577f5127b07317e7e7ee4cc92d60ff30
ru_shalm <ru_shalm@hazimu.com>
2015-11-01 23:08:52 +0900

    add ReplaceDeadMember plugin

---
+++ replace_dead_member/README.md
@@ -0,0 +1,9 @@
+# ReplaceDeadMember MV - 戦闘不能メンバー自動入れ替えさん for MV
+
+Replace a back member from dead actors in BattleScene.
+
+戦闘中、死亡したメンバーを自動的に控えメンバーに入れ替えます。
+
+## Video
+https://youtu.be/cNgG6zAMxO8
+
---
+++ replace_dead_member/Torigoya_ReplaceDeadMember.js
@@ -0,0 +1,60 @@
+//=============================================================================
+// Torigoya_ReplaceDeadMember.js
+//=============================================================================
+
+/*:
+ * @plugindesc Replace a back member from dead actors in BattleScene.
+ * @author ru_shalm
+ */
+
+/*:ja
+ * @plugindesc 戦闘中、死亡したメンバーを自動的に控えメンバーに入れ替えます。
+ * @author ru_shalm
+ */
+
+(function (global) {
+    // 生存メンバーが先頭に集まるように並び替え
+    var sortDeadMember = function () {
+        var allMembers = $gameParty.allMembers();
+        for (var i = 0; i < allMembers.length - 1; ++i) {
+            if (!allMembers[i].isDead()) continue;
+
+            for (var j = i + 1; j < allMembers.length; ++j) {
+                if (allMembers[j].isDead()) continue;
+                $gameParty.swapOrder(i, j);
+                allMembers = $gameParty.allMembers(); // 取り直す
+                break;
+            }
+        }
+    };
+
+    // 死んだ戦闘参加中メンバーを控えと入れ替え
+    var swapDeadMember = function () {
+        var allMembers = $gameParty.allMembers();
+        for (var i = 0; i < $gameParty.maxBattleMembers(); ++i) {
+            if (!allMembers[i].isDead()) continue;
+
+            for (var j = $gameParty.maxBattleMembers(); j < allMembers.length; ++j) {
+                if (allMembers[j].isDead()) continue;
+                $gameParty.swapOrder(i, j);
+                allMembers = $gameParty.allMembers(); // 取り直す
+                break;
+            }
+        }
+    };
+
+    var upstream_BattleManager_initMembers = BattleManager.initMembers;
+    BattleManager.initMembers = function () {
+        sortDeadMember();
+        upstream_BattleManager_initMembers.bind(this)();
+    };
+
+    var upstream_BattleManager_endAction = BattleManager.endAction;
+    BattleManager.endAction = function () {
+        swapDeadMember();
+        upstream_BattleManager_endAction.bind(this)();
+    };
+
+    global.Torigoya = (global.Torigoya || {});
+    global.Torigoya.ReplaceDeadMember = {};
+})(this);
