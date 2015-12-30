a8cedfe27d76b6a0c8a9d17b0cb0d8e042f96af9
ru_shalm <ru_shalm@hazimu.com>
2015-12-31 01:17:49 +0900

    fix bestEquipItem and clearEquipments

--- utils/Torigoya_NotRemoveWeapon.js
+++ utils/Torigoya_NotRemoveWeapon.js
@@ -13,14 +13,14 @@
  */
 
 (function () {
-    var isWeaponSlot = function () {
-        return (this._actor && this._actor.equipSlots()[this._slotId] === 1);
+    var isWeaponSlot = function (actor, slotId) {
+        return (actor && actor.equipSlots()[slotId] === 1);
     };
 
     // 武器のときは末尾空白を入れない
     var upstream_Window_EquipItem_includes = Window_EquipItem.prototype.includes;
     Window_EquipItem.prototype.includes = function (item) {
-        if (!item && isWeaponSlot.apply(this)) {
+        if (!item && isWeaponSlot(this._actor, this._slotId)) {
             return false;
         }
         return upstream_Window_EquipItem_includes.apply(this, arguments);
@@ -29,9 +29,34 @@
     // 武器のときは空白を選択できない
     var upstream_Window_EquipItem_isEnabled = Window_EquipItem.prototype.isEnabled;
     Window_EquipItem.prototype.isEnabled = function (item) {
-        if (!item && isWeaponSlot.apply(this)) {
+        if (!item && isWeaponSlot(this._actor, this._slotId)) {
             return false;
         }
         return upstream_Window_EquipItem_isEnabled.apply(this, arguments);
+    };
+
+    // [再定義] 全部外すで外れないようにする
+    Game_Actor.prototype.clearEquipments = function () {
+        var maxSlots = this.equipSlots().length;
+        for (var i = 0; i < maxSlots; i++) {
+            if (!isWeaponSlot(this, i) && this.isEquipChangeOk(i)) {
+                this.changeEquip(i, null);
+            }
+        }
+    };
+
+    // 今の装備品とも比較するようにする
+    // ※デフォだと事前に装備を解除することが前提になっていて、その作りは今は困る…
+    var upstream_Game_Actor_bestEquipItem = Game_Actor.prototype.bestEquipItem;
+    Game_Actor.prototype.bestEquipItem = function (slotId) {
+        var bestItem = upstream_Game_Actor_bestEquipItem.apply(this, arguments),
+            nowItem = this._equips[slotId].object(),
+            bestPerformance = bestItem ? this.calcEquipItemPerformance(bestItem) : -1000,
+            nowPerformance = nowItem ? this.calcEquipItemPerformance(nowItem) : -1000;
+        if (nowPerformance > bestPerformance) {
+            return nowItem;
+        } else {
+            return bestItem;
+        }
     };
 })();
