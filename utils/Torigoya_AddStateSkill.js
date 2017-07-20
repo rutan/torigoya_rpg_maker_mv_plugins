43cdbc1ce2e183ed68da15b9190b0f3dd10a3956
ru_shalm <ru_shalm@hazimu.com>
2017-07-21 01:56:04 +0900

    addable multiple state

--- utils/Torigoya_AddStateSkill.js
+++ utils/Torigoya_AddStateSkill.js
@@ -1,6 +1,9 @@
-//=============================================================================
-// Torigoya_AddStateSkill.js
-//=============================================================================
+/*---------------------------------------------------------------------------*
+ * Torigoya_AddStateSkill.js
+ *---------------------------------------------------------------------------*
+ * 2017/07/21 ru_shalm
+ * http://torigoya.hatenadiary.jp/
+ *---------------------------------------------------------------------------*/
 
 /*:
  * @plugindesc add/remove state to subject when using the skill
@@ -21,16 +24,23 @@
  *
  * スキルのメモ:
  *   <AddState:1>     # スキル使用時にステート1番(戦闘不能)を付与
+ *   <AddState:2,3>   # スキル使用時にステート2番と3番を付与
  *   <RemoveState:2>  # スキル使用時にステート2番(防御)を解除
  */
 
 (function (global) {
-    var AddStateSkill = {};
+    'use strict';
+
+    var AddStateSkill = {
+        name: 'Torigoya_AddStateSkill'
+    };
 
     AddStateSkill.addState = function () {
-        var stateId = this._action.item() ? this._action.item().meta['AddState'] : null;
-        if (stateId && this._subject.isAlive()) {
-            this._subject.addState(~~stateId);
+        var stateIds = this._action.item() ? this._action.item().meta['AddState'] : null;
+        if (stateIds && this._subject.isAlive()) {
+            stateIds.split(/\s*,\s*/).filter(Boolean).forEach(function (id) {
+                this._subject.addState(~~id);
+            }.bind(this));
             return 1;
         } else {
             return 0;
@@ -38,9 +48,11 @@
     };
 
     AddStateSkill.removeState = function () {
-        var stateId = this._action.item() ? this._action.item().meta['RemoveState'] : null;
-        if (stateId) {
-            this._subject.removeState(~~stateId);
+        var stateIds = this._action.item() ? this._action.item().meta['RemoveState'] : null;
+        if (stateIds) {
+            stateIds.split(/\s*,\s*/).filter(Boolean).forEach(function (id) {
+                this._subject.removeState(~~id);
+            }.bind(this));
             return 1;
         } else {
             return 0;
@@ -51,7 +63,6 @@
     BattleManager.endAction = function () {
         var n = AddStateSkill.addState.apply(this) + AddStateSkill.removeState.apply(this);
         if (n > 0) {
-            console.log(n);
             this._logWindow.displayAutoAffectedStatus(this._subject);
             if (this._subject.isDead()) {
                 this._subject.performCollapse();
@@ -64,4 +75,4 @@
     // -------------------------------------------------------------------------
     global.Torigoya = (global.Torigoya || {});
     global.Torigoya.AddStateSkill = AddStateSkill;
-})(this);
+})(window);
